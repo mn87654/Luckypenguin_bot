8aa0c7d8e7659ff8b664cb9f59993836ee2baae5
mn87654 <mn8765432oo@gmail.com>
2025-08-29 14:51:19 +0530

    Update main.js

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,13 +1,18 @@
+// webapp/src/main.js
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-
+// Telegram WebApp init
 const tg = window.Telegram?.WebApp
 if (tg) {
-tg.expand()
-tg.ready()
+  tg.expand()
+  tg.ready()
 }
 
-
-createRoot(document.getElementById('root')).render(<App />)
+// Render React App
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
