ee333d07bbdc49bb058ad3fb3e08398ddad79eee
mn87654 <mn8765432oo@gmail.com>
2025-08-29 15:04:15 +0530

    Update main.jsx

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,18 +1,18 @@
-// webapp/src/main.js
+// webapp/src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './App'   // App.jsx को ऑटो-रिज़ॉल्व कर लेगा
 
-// Telegram WebApp init
+// Telegram WebApp init (optional)
 const tg = window.Telegram?.WebApp
 if (tg) {
   tg.expand()
   tg.ready()
 }
 
-// Render React App
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
