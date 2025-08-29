e9244ec4473d01414b4bc04f4768509e619fa886
mn87654 <mn8765432oo@gmail.com>
2025-08-29 12:17:54 +0530

    Update vite.config.js

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/webapp/',
-  build: { outDir: 'dist' },
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  server: {
+    host: true
+  }
 })
+
